2d140e181a1c7560553813e53ff84ca8c7510a9a
Royce <royce_nguyen@yahoo.com>
2020-10-15 13:09:55 -0400

    initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+const selectElement = function(element){
+  return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.hamburger');
+let header = selectElement('header');
+
+menuToggler.addEventListener('click', function(){
+  header.classList.toggle('open')
+})
