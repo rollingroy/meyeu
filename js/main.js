ba4761b86ed5de5ffb6ef1bd38419a5b527d8ae4
Vy <ngdepcondethuong@yahoo.com>
2020-10-15 17:18:04 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,61 @@
-const selectElement = function(element){
-  return document.querySelector(element);
+$(document).ready(function() {
+	$(".hamburger").on('click', function() {
+    $("header").toggleClass("open");
+  });
+  $("a").on('click', function(){
+    $("header").removeClass('open');
+  })
+});
+
+
+// Scroll to top button //
+
+//Get the button:
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
 };
 
-let menuToggler = selectElement('.hamburger');
-let header = selectElement('header');
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-menuToggler.addEventListener('click', function(){
-  header.classList.toggle('open')
-})
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $('a[href*="#"]').on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        "linear",
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
